65489f3ce7e56446009283754d9cb52967dc23ac
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2020-10-13 13:17:29 +0200

    Draft: field names in lists (unused)

--- src/RecordItem.js
+++ src/RecordItem.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import 'typeface-roboto';
 
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import ListItem from '@material-ui/core/ListItem';
-
+import ListItemText from '@material-ui/core/ListItemText';
 
 export default function RecordItem(props) {
   return (
     <ListItem alignItems="flex-start">
-      <Typography component="p">
+      <ListItemText 
+        primary={props.itemText} 
+        primaryTypographyProps = {{component: "p"}}
+        // secondary={props.itemLabel} 
+        // secondaryTypographyProps = {{variant: "caption"}}
+      />
+      {/* <Typography component="p">
         {props.itemText}
-      </Typography>
+      </Typography> */}
     </ListItem>
 
   );
